5a8eaf175dc650da61758632c7ef91e5cce790ec
Ilya Shpigor <petrsum@gmail.com>
2018-10-17 20:56:44 +0200

    * Store in the state arrays numbers instead of bools * Fix the evaluation function

--- bots/utility/game_state.lua
+++ bots/utility/game_state.lua
@@ -9,6 +9,8 @@ local env = require(
 
 local M = {}
 
+local BOOL_TO_NUMBER = {  [true] = 1, [false] = 0 }
+
 M.BOT_STATE = {}
 M.ENEMY_HERO_STATE = {}
 M.ALLY_TOWER_STATE = {}
@@ -17,46 +19,46 @@ M.CREEPS_STATE = {}
 
 function M.UpdateState()
   M.BOT_STATE = {
-    [1] = algorithms.IsBotAlive(),
-    [2] = env.IS_BOT_LOW_HP,
-    [3] = env.BOT_DATA.is_flask_healing,
-    [4] = env.BOT_DATA.is_healing,
-    [5] = env.IS_BASE_RECOVERY,
-    [6] = env.DOES_BOT_HAVE_ADVANTAGE,
-    [7] = env.IS_FOCUSED_BY_CREEPS,
-    [8] = env.IS_FOCUSED_BY_ENEMY_HERO,
-    [9] = env.IS_FOCUSED_BY_UNKNOWN_UNIT,
-    [10] = env.IS_FOCUSED_BY_TOWER
+    [1] = BOOL_TO_NUMBER[algorithms.IsBotAlive()],
+    [2] = BOOL_TO_NUMBER[env.IS_BOT_LOW_HP],
+    [3] = BOOL_TO_NUMBER[env.BOT_DATA.is_flask_healing],
+    [4] = BOOL_TO_NUMBER[env.BOT_DATA.is_healing],
+    [5] = BOOL_TO_NUMBER[env.IS_BASE_RECOVERY],
+    [6] = BOOL_TO_NUMBER[env.DOES_BOT_HAVE_ADVANTAGE],
+    [7] = BOOL_TO_NUMBER[env.IS_FOCUSED_BY_CREEPS],
+    [8] = BOOL_TO_NUMBER[env.IS_FOCUSED_BY_ENEMY_HERO],
+    [9] = BOOL_TO_NUMBER[env.IS_FOCUSED_BY_UNKNOWN_UNIT],
+    [10] = BOOL_TO_NUMBER[env.IS_FOCUSED_BY_TOWER]
   }
 
   M.ENEMY_HERO_STATE = {
-    env.ENEMY_HERO_DATA ~= nil,
-    env.IS_ENEMY_HERO_LOW_HP,
-    env.DOES_TOWER_PROTECT_ENEMY,
-    env.ENEMY_HERO_DISTANCE,
-    env.DOES_ENEMY_HERO_HAVE_ADVANTAGE,
+    [1] = BOOL_TO_NUMBER[env.ENEMY_HERO_DATA ~= nil],
+    [2] = BOOL_TO_NUMBER[env.IS_ENEMY_HERO_LOW_HP],
+    [3] = BOOL_TO_NUMBER[env.DOES_TOWER_PROTECT_ENEMY],
+    [4] = env.ENEMY_HERO_DISTANCE,
+    [5] = BOOL_TO_NUMBER[env.DOES_ENEMY_HERO_HAVE_ADVANTAGE],
   }
 
   M.ALLY_TOWER_STATE = {
-    env.ALLY_TOWER_DATA ~= nil,
+    [1] = BOOL_TO_NUMBER[env.ALLY_TOWER_DATA ~= nil],
   }
 
   M.ENEMY_TOWER_STATE = {
-    env.ENEMY_TOWER_DATA ~= nil,
-    env.ENEMY_TOWER_DISTANCE,
+    [1] = BOOL_TO_NUMBER[env.ENEMY_TOWER_DATA ~= nil],
+    [2] = env.ENEMY_TOWER_DISTANCE,
   }
 
   M.CREEPS_STATE = {
-    env.ENEMY_CREEP_FRONT_DATA ~= nil,
-    env.ENEMY_CREEP_BACK_DATA ~= nil,
-    env.ALLY_CREEP_FRONT_DATA ~= nil,
-    env.ALLY_CREEP_BACK_DATA ~= nil,
-    env.PRE_LAST_HIT_ENEMY_CREEP ~= nil,
-    env.PRE_LAST_HIT_ALLY_CREEP ~= nil,
-    env.LAST_HIT_ENEMY_CREEP ~= nil,
-    env.LAST_HIT_ALLY_CREEP ~= nil,
-    env.ALLY_CREEPS_HP,
-    env.ENEMY_CREEPS_HP,
+    [1] = BOOL_TO_NUMBER[env.ENEMY_CREEP_FRONT_DATA ~= nil],
+    [2] = BOOL_TO_NUMBER[env.ENEMY_CREEP_BACK_DATA ~= nil],
+    [3] = BOOL_TO_NUMBER[env.ALLY_CREEP_FRONT_DATA ~= nil],
+    [4] = BOOL_TO_NUMBER[env.ALLY_CREEP_BACK_DATA ~= nil],
+    [5] = BOOL_TO_NUMBER[env.PRE_LAST_HIT_ENEMY_CREEP ~= nil],
+    [6] = BOOL_TO_NUMBER[env.PRE_LAST_HIT_ALLY_CREEP ~= nil],
+    [7] = BOOL_TO_NUMBER[env.LAST_HIT_ENEMY_CREEP ~= nil],
+    [8] = BOOL_TO_NUMBER[env.LAST_HIT_ALLY_CREEP ~= nil],
+    [9] = env.ALLY_CREEPS_HP,
+    [10] = env.ENEMY_CREEPS_HP,
   }
 end
 
@@ -66,8 +68,8 @@ function M.Evaluate(state, weights)
   functions.DoWithKeysAndElements(
     weights,
     function(key, weight)
-      if state[key] ~= nil and state[key] then
-        result = result + weight
+      if state[key] ~= nil then
+        result = result + state[key] * weight
       end
     end)
 
