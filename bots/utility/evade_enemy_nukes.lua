229b5bd2e4c7944cf78a5e2bbf60a77fc8cf1fb3
Ilya Shpigor <petrsum@gmail.com>
2018-11-17 18:49:32 +0100

    Base implementation of the "evade_enemy_nukes" objective

--- bots/utility/evade_enemy_nukes.lua
+++ bots/utility/evade_enemy_nukes.lua
@@ -1,3 +1,6 @@
+local constants = require(
+  GetScriptDirectory() .."/utility/constants")
+
 local action_timing = require(
   GetScriptDirectory() .."/utility/action_timing")
 
@@ -19,6 +22,90 @@ function M.evade_enemy_nukes()
 end
 
 ---------------------------------
+
+function M.pre_evade_near_shadowraze_back()
+  local weights = {
+    [gs.EH_IS_CASTING_NEAR_SHADOWRAZE] = 0.5,
+    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 0.5,
+    [gs.BOT_IS_FACING_EH] = -1,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+---------------------------------
+
+function M.pre_evade_medium_shadowraze_back()
+  local weights = {
+    [gs.EH_IS_CASTING_MEDIUM_SHADOWRAZE] = 0.5,
+    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 0.5,
+    [gs.BOT_IS_FACING_EH] = -1,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+---------------------------------
+
+function M.pre_evade_far_shadowraze_back()
+  local weights = {
+    [gs.EH_IS_CASTING_FAR_SHADOWRAZE] = 0.5,
+    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 0.5,
+    [gs.BOT_IS_FACING_EH] = -1,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+function M.move_back()
+  env.BOT:Action_MoveDirectly(env.SAFE_SPOT)
+
+  action_timing.SetNextActionDelay(
+    constants.NEVERMORE_SHADOWRAZE_CAST_TIME)
+end
+
+---------------------------------
+
+function M.pre_evade_near_shadowraze_front()
+  local weights = {
+    [gs.EH_IS_CASTING_NEAR_SHADOWRAZE] = 0.3,
+    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 0.3,
+    [gs.BOT_IS_FACING_EH] = 0.4,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+---------------------------------
+
+function M.pre_evade_medium_shadowraze_back()
+  local weights = {
+    [gs.EH_IS_CASTING_MEDIUM_SHADOWRAZE] = 0.3,
+    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 0.3,
+    [gs.BOT_IS_FACING_EH] = 0.4,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+---------------------------------
+
+function M.pre_evade_far_shadowraze_back()
+  local weights = {
+    [gs.EH_IS_CASTING_FAR_SHADOWRAZE] = 0.3,
+    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 0.3,
+    [gs.BOT_IS_FACING_EH] = 0.4,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+function M.move_front()
+  moves.move_enemy_hero()
+
+  action_timing.SetNextActionDelay(
+    constants.NEVERMORE_SHADOWRAZE_CAST_TIME)
+end
 
 -- Provide an access to local functions for unit tests only
 
