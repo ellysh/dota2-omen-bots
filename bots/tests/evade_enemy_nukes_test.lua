15b041ad03e125ffc91651254c33b4f57ee38797
Ilya Shpigor <petrsum@gmail.com>
2018-11-17 18:20:10 +0100

    Initial add the "evade_enemy_nukes" objective

---
+++ bots/tests/evade_enemy_nukes_test.lua
@@ -0,0 +1,225 @@
+package.path = package.path .. ";../utility/?.lua"
+
+pcall(require, "luacov")
+require("global_functions")
+
+local evasion = require("evasion")
+local gs = require("game_state")
+local luaunit = require("luaunit")
+
+---------------------------------
+
+function test_pre_move_safe_recovery_1_succeed()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_FLASK_HEALING] = 1,
+    [gs.BOT_HAS_MODIFIER_FOUNTAIN] = 0,
+    [gs.BOT_HP_MAX_DELTA] = 0.09,
+    [gs.BOT_IN_SAFE_SPOT] = 0,
+  })
+
+  luaunit.assertTrue(evasion.pre_move_safe_recovery())
+end
+
+function test_pre_move_safe_recovery_1_fails()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_FLASK_HEALING] = 1,
+    [gs.BOT_HAS_MODIFIER_FOUNTAIN] = 0,
+    [gs.BOT_HP_MAX_DELTA] = 0.07,
+    [gs.BOT_IN_SAFE_SPOT] = 0,
+  })
+
+  luaunit.assertFalse(evasion.pre_move_safe_recovery())
+end
+
+function test_pre_move_safe_recovery_2_fails()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_FLASK_HEALING] = 0,
+    [gs.BOT_HAS_MODIFIER_FOUNTAIN] = 0,
+    [gs.BOT_HP_MAX_DELTA] = 1,
+    [gs.BOT_IN_SAFE_SPOT] = 0,
+  })
+
+  luaunit.assertFalse(evasion.pre_move_safe_recovery())
+end
+
+function test_pre_move_safe_recovery_3_fails()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_FLASK_HEALING] = 1,
+    [gs.BOT_HAS_MODIFIER_FOUNTAIN] = 1,
+    [gs.BOT_HP_MAX_DELTA] = 0.1,
+    [gs.BOT_IN_SAFE_SPOT] = 0,
+  })
+
+  luaunit.assertFalse(evasion.pre_move_safe_recovery())
+end
+
+function test_pre_move_safe_recovery_4_fails()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_FLASK_HEALING] = 1,
+    [gs.BOT_HAS_MODIFIER_FOUNTAIN] = 0,
+    [gs.BOT_HP_MAX_DELTA] = 0.1,
+    [gs.BOT_IN_SAFE_SPOT] = 1,
+  })
+
+  luaunit.assertFalse(evasion.pre_move_safe_recovery())
+end
+
+---------------------------------
+
+function test_pre_evade_enemy_hero_1_succeed()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_LOW_HP] = 1,
+    [gs.BOT_IS_FOCUSED_BY_ENEMY_HERO] = 0,
+    [gs.BOT_IS_FOCUSED_BY_UNKNOWN_UNIT] = 0,
+    [gs.EC_IN_AGGRO_RADIUS] = 0,
+  })
+
+  luaunit.assertTrue(evasion.pre_evade_enemy_hero())
+end
+
+function test_pre_evade_enemy_hero_2_succeed()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_LOW_HP] = 0,
+    [gs.BOT_IS_FOCUSED_BY_ENEMY_HERO] = 1,
+    [gs.BOT_IS_FOCUSED_BY_UNKNOWN_UNIT] = 0,
+    [gs.EC_IN_AGGRO_RADIUS] = 1,
+  })
+
+  luaunit.assertTrue(evasion.pre_evade_enemy_hero())
+end
+
+function test_pre_evade_enemy_hero_3_succeed()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_LOW_HP] = 0,
+    [gs.BOT_IS_FOCUSED_BY_ENEMY_HERO] = 0,
+    [gs.BOT_IS_FOCUSED_BY_UNKNOWN_UNIT] = 1,
+    [gs.EC_IN_AGGRO_RADIUS] = 1,
+  })
+
+  luaunit.assertTrue(evasion.pre_evade_enemy_hero())
+end
+
+function test_pre_evade_enemy_hero_1_fails()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_LOW_HP] = 0,
+    [gs.BOT_IS_FOCUSED_BY_ENEMY_HERO] = 1,
+    [gs.BOT_IS_FOCUSED_BY_UNKNOWN_UNIT] = 0,
+    [gs.EC_IN_AGGRO_RADIUS] = 0,
+  })
+
+  luaunit.assertFalse(evasion.pre_evade_enemy_hero())
+end
+
+function test_pre_evade_enemy_hero_2_fails()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_LOW_HP] = 0,
+    [gs.BOT_IS_FOCUSED_BY_ENEMY_HERO] = 0,
+    [gs.BOT_IS_FOCUSED_BY_UNKNOWN_UNIT] = 1,
+    [gs.EC_IN_AGGRO_RADIUS] = 0,
+  })
+
+  luaunit.assertFalse(evasion.pre_evade_enemy_hero())
+end
+
+function test_pre_evade_enemy_hero_3_fails()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_LOW_HP] = 0,
+    [gs.BOT_IS_FOCUSED_BY_ENEMY_HERO] = 0,
+    [gs.BOT_IS_FOCUSED_BY_UNKNOWN_UNIT] = 0,
+    [gs.EC_IN_AGGRO_RADIUS] = 1,
+  })
+
+  luaunit.assertFalse(evasion.pre_evade_enemy_hero())
+end
+
+---------------------------------
+
+function test_pre_evade_enemy_creeps_1_succeed()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_FOCUSED_BY_CREEPS] = 1,
+  })
+
+  luaunit.assertTrue(evasion.pre_evade_enemy_creeps())
+end
+
+function test_pre_evade_enemy_creeps_1_fails()
+  evasion.test_SetGameState({
+    [gs.BOT_IS_FOCUSED_BY_CREEPS] = 0,
+  })
+
+  luaunit.assertFalse(evasion.pre_evade_enemy_creeps())
+end
+
+---------------------------------
+
+function test_pre_evade_enemy_tower_1_succeed()
+  evasion.test_SetGameState({
+    -- weights_1
+    [gs.BOT_IS_FOCUSED_BY_TOWER] = 1,
+    [gs.BOT_IN_ET_MIN_DISTANCE] = 0,
+
+    -- weights_2
+    [gs.BOT_HAS_LEVEL_FOR_AGRESSION] = 0,
+    [gs.BOT_IN_ENEMY_TOWER_RANGE] = 0,
+  })
+
+  luaunit.assertTrue(evasion.pre_evade_enemy_tower())
+end
+
+function test_pre_evade_enemy_tower_2_succeed()
+  evasion.test_SetGameState({
+    -- weights_1
+    [gs.BOT_IS_FOCUSED_BY_TOWER] = 0,
+    [gs.BOT_IN_ET_MIN_DISTANCE] = 1,
+
+    -- weights_2
+    [gs.BOT_HAS_LEVEL_FOR_AGRESSION] = 0,
+    [gs.BOT_IN_ENEMY_TOWER_RANGE] = 0,
+  })
+
+  luaunit.assertTrue(evasion.pre_evade_enemy_tower())
+end
+
+function test_pre_evade_enemy_tower_3_succeed()
+  evasion.test_SetGameState({
+    -- weights_1
+    [gs.BOT_IS_FOCUSED_BY_TOWER] = 0,
+    [gs.BOT_IN_ET_MIN_DISTANCE] = 0,
+
+    -- weights_2
+    [gs.BOT_HAS_LEVEL_FOR_AGRESSION] = 0,
+    [gs.BOT_IN_ENEMY_TOWER_RANGE] = 1,
+  })
+
+  luaunit.assertTrue(evasion.pre_evade_enemy_tower())
+end
+
+function test_pre_evade_enemy_tower_1_fails()
+  evasion.test_SetGameState({
+    -- weights_1
+    [gs.BOT_IS_FOCUSED_BY_TOWER] = 0,
+    [gs.BOT_IN_ET_MIN_DISTANCE] = 0,
+
+    -- weights_2
+    [gs.BOT_HAS_LEVEL_FOR_AGRESSION] = 0,
+    [gs.BOT_IN_ENEMY_TOWER_RANGE] = 0,
+  })
+
+  luaunit.assertFalse(evasion.pre_evade_enemy_tower())
+end
+
+function test_pre_evade_enemy_tower_2_fails()
+  evasion.test_SetGameState({
+    -- weights_1
+    [gs.BOT_IS_FOCUSED_BY_TOWER] = 0,
+    [gs.BOT_IN_ET_MIN_DISTANCE] = 0,
+
+    -- weights_2
+    [gs.BOT_HAS_LEVEL_FOR_AGRESSION] = 1,
+    [gs.BOT_IN_ENEMY_TOWER_RANGE] = 1,
+  })
+
+  luaunit.assertFalse(evasion.pre_evade_enemy_tower())
+end
+
+os.exit(luaunit.LuaUnit.run())
---
+++ bots/utility/evade_enemy_nukes.lua
@@ -0,0 +1,103 @@
+local action_timing = require(
+  GetScriptDirectory() .."/utility/action_timing")
+
+local moves = require(
+  GetScriptDirectory() .."/utility/moves")
+
+local env = require(
+  GetScriptDirectory() .."/utility/environment")
+
+local gs = require(
+  GetScriptDirectory() .."/utility/game_state")
+
+local M = {}
+
+---------------------------------
+
+function M.pre_evasion()
+  return moves.pre_interrupt_cast_objective()
+end
+
+---------------------------------
+
+local function GetFlaskHealingRemainingDuration()
+  local modifier = env.BOT:GetModifierByName("modifier_flask_healing")
+  return env.BOT:GetModifierRemainingDuration(modifier)
+end
+
+function M.pre_move_safe_recovery()
+  local weights = {
+    [gs.BOT_IS_FLASK_HEALING] = 0.924,
+    [gs.BOT_HAS_MODIFIER_FOUNTAIN] = -1,
+    [gs.BOT_HP_MAX_DELTA] = 0.95,
+    [gs.BOT_IN_SAFE_SPOT] = -1,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+function M.move_safe_recovery()
+  env.BOT:Action_MoveDirectly(env.SAFE_SPOT)
+
+  action_timing.SetNextActionDelay(0.1)
+end
+
+---------------------------------
+
+function M.pre_evade_enemy_hero()
+  local weights = {
+    [gs.BOT_IS_LOW_HP] = 1,
+    [gs.BOT_IS_FOCUSED_BY_ENEMY_HERO] = 0.5,
+    [gs.BOT_IS_FOCUSED_BY_UNKNOWN_UNIT] = 0.5,
+    [gs.EC_IN_AGGRO_RADIUS] = 0.5,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+function M.evade_enemy_hero()
+  env.BOT:Action_MoveDirectly(env.SAFE_SPOT)
+end
+
+---------------------------------
+
+function M.pre_evade_enemy_creeps()
+  local weights = {
+    [gs.BOT_IS_FOCUSED_BY_CREEPS] = 1,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights)
+end
+
+function M.evade_enemy_creeps()
+  env.BOT:Action_MoveDirectly(env.SAFE_SPOT)
+end
+
+---------------------------------
+
+function M.pre_evade_enemy_tower()
+  local weights_1 = {
+    [gs.BOT_IS_FOCUSED_BY_TOWER] = 1,
+    [gs.BOT_IN_ET_MIN_DISTANCE] = 1,
+  }
+
+  local weights_2 = {
+    [gs.BOT_HAS_LEVEL_FOR_AGRESSION] = -1,
+    [gs.BOT_IN_ENEMY_TOWER_RANGE] = 1,
+  }
+
+  return gs.Evaluate(gs.GAME_STATE, weights_1)
+         or gs.Evaluate(gs.GAME_STATE, weights_2)
+end
+
+function M.evade_enemy_tower()
+  env.BOT:Action_MoveDirectly(env.SAFE_SPOT)
+end
+
+-- Provide an access to local functions for unit tests only
+
+function M.test_SetGameState(state)
+  gs.GAME_STATE = state
+end
+
+return M
