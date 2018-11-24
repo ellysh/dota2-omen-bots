515d54050cb7977381b662cc9b6790986bfc0992
Ilya Shpigor <petrsum@gmail.com>
2018-11-24 17:55:31 +0100

    * Initial add the cancel_current_move objective * Remove the cancel_turn objective

--- bots/tests/cancel_current_move_test.lua
+++ bots/tests/cancel_current_move_test.lua
@@ -3,7 +3,7 @@ package.path = package.path .. ";../utility/?.lua"
 pcall(require, "luacov")
 require("global_functions")
 
-local cancel_turn = require("cancel_turn")
+local cancel_current_move = require("cancel_current_move")
 local gs = require("game_state")
 local luaunit = require("luaunit")
 
