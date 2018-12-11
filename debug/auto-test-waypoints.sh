6e1065ec8abc7bda980afefaaef1313f4ad6c504
Ilya Shpigor <petrsum@gmail.com>
2018-12-11 19:52:27 +0100

    Add the script for debugging waypoints

---
+++ debug/auto-test-waypoints.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sleep 3
+
+# Give items, gold and level to the bot
+
+xdotool type 'dota_bot_give_level 1'
+xdotool key 'Return'
+
+xdotool type 'dota_bot_give_item item_shivas_guard'
+xdotool key 'Return'
+
+xdotool type 'dota_bot_give_item item_shivas_guard'
+xdotool key 'Return'
+
+xdotool type 'dota_bot_give_item item_heart'
+xdotool key 'Return'
+
+xdotool type 'dota_bot_give_item item_travel_boots_2'
+xdotool key 'Return'
+
+xdotool type 'dota_bot_give_item item_hood_of_defiance'
+xdotool key 'Return'
