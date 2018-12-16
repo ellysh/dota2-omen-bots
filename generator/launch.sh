22d199d71f139813072fc760596e4489e1151ce6
Ilya Shpigor <petrsum@gmail.com>
2018-12-16 20:10:25 +0100

    Add the "assault_enemy_tower" strategy to the database

--- generator/launch.sh
+++ generator/launch.sh
@@ -9,9 +9,15 @@ RESULT_DIR="../bots/database"
 ./ods2csv.sh
 
 ./generator.py $CSV_DIR/buy.csv > $LUA_DIR/buy.lua
-./generator.py $CSV_DIR/farm.csv > $LUA_DIR/farm.lua
 ./generator.py $CSV_DIR/recovery.csv > $LUA_DIR/recovery.lua
+./generator.py $CSV_DIR/farm.csv > $LUA_DIR/farm.lua
+./generator.py $CSV_DIR/assault_enemy_tower.csv > $LUA_DIR/assault_enemy_tower.lua
 
-./linker.py $LUA_DIR/buy.lua $LUA_DIR/recovery.lua $LUA_DIR/farm.lua > $RESULT_DIR/objectives.lua
+./linker.py \
+  $LUA_DIR/buy.lua \
+  $LUA_DIR/recovery.lua \
+  $LUA_DIR/farm.lua \
+  $LUA_DIR/assault_enemy_tower.lua \
+  > $RESULT_DIR/objectives.lua
 
 ./check.sh
--- generator/ods2csv.sh
+++ generator/ods2csv.sh
@@ -9,3 +9,4 @@ ssconvert -S -O 'separator=;' "$DATABASE_DIR/Database.ods" "$TEMP_DIR/Database.t
 cp "$TEMP_DIR/Database.txt.0" "$CSV_DIR/buy.csv"
 cp "$TEMP_DIR/Database.txt.1" "$CSV_DIR/recovery.csv"
 cp "$TEMP_DIR/Database.txt.2" "$CSV_DIR/farm.csv"
+cp "$TEMP_DIR/Database.txt.2" "$CSV_DIR/assault_enemy_tower.csv"
