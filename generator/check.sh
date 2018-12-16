2e1a5a96a11403d8c16b8abb04d8f77d6e7a5e80
Ilya Shpigor <petrsum@gmail.com>
2018-12-16 13:25:44 +0100

    Remove the defensive and offensive strategies

--- generator/check.sh
+++ generator/check.sh
@@ -9,7 +9,5 @@ FIRST_RUN=0
 ./validator.py $CSV_DIR/buy.csv $DICTIONARY $FIRST_RUN
 ./validator.py $CSV_DIR/farm.csv $DICTIONARY $FIRST_RUN
 ./validator.py $CSV_DIR/recovery.csv $DICTIONARY $FIRST_RUN
-./validator.py $CSV_DIR/offensive.csv $DICTIONARY $FIRST_RUN
-./validator.py $CSV_DIR/defensive.csv $DICTIONARY $FIRST_RUN
 
 sort -u -o $DICTIONARY $DICTIONARY
