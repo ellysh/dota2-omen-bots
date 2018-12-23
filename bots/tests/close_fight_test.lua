acbf619e3f1a1a4abe0bf17c4592b2a32d288537
Ilya Shpigor <petrsum@gmail.com>
2018-12-23 23:09:34 +0100

    Add unit tests for the "close_fight" objective

--- bots/tests/close_fight_test.lua
+++ bots/tests/close_fight_test.lua
@@ -9,4 +9,60 @@ local luaunit = require("luaunit")
 
 ---------------------------------
 
+function test_pre_close_fight_1_succeed()
+  close_fight.test_SetGameState({
+    [gs.BOT_IS_CASTING] = 0,
+    [gs.EH_IS_VISIBLE] = 1,
+  })
+
+  luaunit.assertTrue(close_fight.pre_close_fight())
+end
+
+function test_pre_close_fight_1_fails()
+  close_fight.test_SetGameState({
+    [gs.BOT_IS_CASTING] = 0,
+    [gs.EH_IS_VISIBLE] = 0,
+  })
+
+  luaunit.assertFalse(close_fight.pre_close_fight())
+end
+
+function test_pre_close_fight_2_fails()
+  close_fight.test_SetGameState({
+    [gs.BOT_IS_CASTING] = 1,
+    [gs.EH_IS_VISIBLE] = 1,
+  })
+
+  luaunit.assertFalse(close_fight.pre_close_fight())
+end
+
+---------------------------------
+
+function test_pre_kill_enemy_hero_1_succeed()
+  close_fight.test_SetGameState({
+    [gs.EH_HAS_LESS_HTD] = 1,
+    [gs.EH_NOT_IN_BOT_ATTACK_RANGE] = 0,
+  })
+
+  luaunit.assertTrue(close_fight.pre_kill_enemy_hero())
+end
+
+function test_pre_kill_enemy_hero_1_fails()
+  close_fight.test_SetGameState({
+    [gs.EH_HAS_LESS_HTD] = 0,
+    [gs.EH_NOT_IN_BOT_ATTACK_RANGE] = 0,
+  })
+
+  luaunit.assertFalse(close_fight.pre_kill_enemy_hero())
+end
+
+function test_pre_kill_enemy_hero_2_fails()
+  close_fight.test_SetGameState({
+    [gs.EH_HAS_LESS_HTD] = 1,
+    [gs.EH_NOT_IN_BOT_ATTACK_RANGE] = 1,
+  })
+
+  luaunit.assertFalse(close_fight.pre_kill_enemy_hero())
+end
+
 os.exit(luaunit.LuaUnit.run())
