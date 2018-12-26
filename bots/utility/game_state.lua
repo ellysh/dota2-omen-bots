225fad18d1833df6500164b57b0c0ba4aa1097a4
Ilya Shpigor <petrsum@gmail.com>
2018-12-26 21:41:16 +0100

    Remove deprecated game states for hood of defiance

--- bots/utility/game_state.lua
+++ bots/utility/game_state.lua
@@ -88,8 +88,8 @@ M.BOT_CASTABLE_WARD = 59
 M.BOT_BUY_WARD_PERIOD_ACHIEVED = 60
 M.BOT_HAS_LEVEL_FOR_NUKES = 61
 M.BOT_IS_INACTIVE = 62
-M.BOT_CASTABLE_HOOD = 63
-M.BOT_HAS_MODIFIER_HOOD = 64
+--M.BOT_CASTABLE_HOOD = 63
+--M.BOT_HAS_MODIFIER_HOOD = 64
 
 -- ENEMY_HERO state
 M.EH_PRESENT = 100
@@ -434,17 +434,6 @@ function M.UpdateState()
 
     [M.BOT_HAS_LEVEL_FOR_NUKES] =
       NUM[constants.HERO_LEVEL_FOR_NUKES <= env.BOT_DATA.level],
-
-    [M.BOT_CASTABLE_HOOD] =
-      NUM[algorithms.IsItemCastable(
-            env.BOT_DATA,
-            "item_hood_of_defiance",
-            false)],
-
-    [M.BOT_HAS_MODIFIER_HOOD] =
-       NUM[algorithms.HasModifier(
-             env.BOT_DATA,
-             "modifier_item_hood_of_defiance_barrier")],
   }
 
   if hist.LAST_SOMA.move ~= nil then
