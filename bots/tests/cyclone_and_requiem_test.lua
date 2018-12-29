911e7708471710642ba515086e26df8fdfaa4350
Ilya Shpigor <petrsum@gmail.com>
2018-12-29 09:00:08 +0100

    Add unit tests for the "cyclone_and_requiem" objective

--- bots/tests/cyclone_and_requiem_test.lua
+++ bots/tests/cyclone_and_requiem_test.lua
@@ -3,482 +3,165 @@ package.path = package.path .. ";../utility/?.lua"
 pcall(require, "luacov")
 require("global_functions")
 
-local nuke_enemy_hero = require("nuke_enemy_hero")
+local cyclone_and_requiem = require("cyclone_and_requiem")
 local gs = require("game_state")
 local luaunit = require("luaunit")
 
 ---------------------------------
 
-function test_pre_nuke_enemy_hero_1_succeed()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.BOT_HAS_LEVEL_FOR_NUKES] = 1,
+function test_pre_cyclone_and_requiem_1_succeed()
+  cyclone_and_requiem.test_SetGameState({
+    [gs.BOT_HAS_MP_FOR_CYCLONE_AND_REQUIEM] = 1,
+    [gs.BOT_HAS_MAX_SOULS] = 1,
+
+    -- M.pre_attack_objective
     [gs.BOT_IS_ALIVE] = 1,
     [gs.BOT_IS_LOW_HP] = 0,
     [gs.BOT_IS_CASTING] = 0,
   })
 
-  luaunit.assertTrue(nuke_enemy_hero.pre_nuke_enemy_hero())
+  luaunit.assertTrue(cyclone_and_requiem.pre_cyclone_and_requiem())
 end
 
-function test_pre_nuke_enemy_hero_1_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.BOT_HAS_LEVEL_FOR_NUKES] = 0,
+function test_pre_cyclone_and_requiem_1_fails()
+  cyclone_and_requiem.test_SetGameState({
+    [gs.BOT_HAS_MP_FOR_CYCLONE_AND_REQUIEM] = 0,
+    [gs.BOT_HAS_MAX_SOULS] = 1,
+
+    -- M.pre_attack_objective
     [gs.BOT_IS_ALIVE] = 1,
     [gs.BOT_IS_LOW_HP] = 0,
     [gs.BOT_IS_CASTING] = 0,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_nuke_enemy_hero())
+  luaunit.assertFalse(cyclone_and_requiem.pre_cyclone_and_requiem())
+end
+
+function test_pre_cyclone_and_requiem_2_fails()
+  cyclone_and_requiem.test_SetGameState({
+    [gs.BOT_HAS_MP_FOR_CYCLONE_AND_REQUIEM] = 1,
+    [gs.BOT_HAS_MAX_SOULS] = 0,
+
+    -- M.pre_attack_objective
+    [gs.BOT_IS_ALIVE] = 1,
+    [gs.BOT_IS_LOW_HP] = 0,
+    [gs.BOT_IS_CASTING] = 0,
+  })
+
+  luaunit.assertFalse(cyclone_and_requiem.pre_cyclone_and_requiem())
 end
 
 ---------------------------------
 
-function test_pre_turn_near_shadowraze_1_succeed()
-  nuke_enemy_hero.test_SetGameState({
+function test_pre_cyclone_1_succeed()
+  cyclone_and_requiem.test_SetGameState({
     [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 0,
-    [gs.BOT_IS_TURNING] = 0,
+    [gs.BOT_CASTABLE_CYCLONE] = 1,
+    [gs.EH_IN_CYCLONE_RANGE] = 1,
   })
 
-  luaunit.assertTrue(nuke_enemy_hero.pre_turn_near_shadowraze())
+  luaunit.assertTrue(cyclone_and_requiem.pre_cyclone())
 end
 
-function test_pre_turn_near_shadowraze_1_fails()
-  nuke_enemy_hero.test_SetGameState({
+function test_pre_cyclone_1_fails()
+  cyclone_and_requiem.test_SetGameState({
     [gs.EH_IS_VISIBLE] = 0,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 0,
-    [gs.BOT_IS_TURNING] = 0,
+    [gs.BOT_CASTABLE_CYCLONE] = 1,
+    [gs.EH_IN_CYCLONE_RANGE] = 1,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_near_shadowraze())
+  luaunit.assertFalse(cyclone_and_requiem.pre_cyclone())
 end
 
-function test_pre_turn_near_shadowraze_2_fails()
-  nuke_enemy_hero.test_SetGameState({
+function test_pre_cyclone_2_fails()
+  cyclone_and_requiem.test_SetGameState({
     [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 0,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 0,
-    [gs.BOT_IS_TURNING] = 0,
+    [gs.BOT_CASTABLE_CYCLONE] = 0,
+    [gs.EH_IN_CYCLONE_RANGE] = 1,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_near_shadowraze())
+  luaunit.assertFalse(cyclone_and_requiem.pre_cyclone())
 end
 
-function test_pre_turn_near_shadowraze_3_fails()
-  nuke_enemy_hero.test_SetGameState({
+function test_pre_cyclone_3_fails()
+  cyclone_and_requiem.test_SetGameState({
     [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 0,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 0,
-    [gs.BOT_IS_TURNING] = 0,
+    [gs.BOT_CASTABLE_CYCLONE] = 1,
+    [gs.EH_IN_CYCLONE_RANGE] = 0,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_near_shadowraze())
-end
-
-function test_pre_turn_near_shadowraze_4_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 1,
-    [gs.BOT_IS_TURNING] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_near_shadowraze())
-end
-
-function test_pre_turn_near_shadowraze_5_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 0,
-    [gs.BOT_IS_TURNING] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_near_shadowraze())
+  luaunit.assertFalse(cyclone_and_requiem.pre_cyclone())
 end
 
 ---------------------------------
 
-function test_pre_turn_medium_shadowraze_1_succeed()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 0,
+function test_pre_move_enemy_hero_location_1_succeed()
+  cyclone_and_requiem.test_SetGameState({
+    [gs.EH_HAS_CYCLONE_MODIFIER] = 1,
+    [gs.BOT_CASTABLE_REQUIEM] = 1,
+    [gs.BOT_IN_EH_LOCATION] = 0,
   })
 
-  luaunit.assertTrue(nuke_enemy_hero.pre_turn_medium_shadowraze())
+  luaunit.assertTrue(cyclone_and_requiem.pre_move_enemy_hero_location())
 end
 
-function test_pre_turn_medium_shadowraze_1_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 0,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 0,
+function test_pre_move_enemy_hero_location_1_fails()
+  cyclone_and_requiem.test_SetGameState({
+    [gs.EH_HAS_CYCLONE_MODIFIER] = 0,
+    [gs.BOT_CASTABLE_REQUIEM] = 1,
+    [gs.BOT_IN_EH_LOCATION] = 0,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_medium_shadowraze())
+  luaunit.assertFalse(cyclone_and_requiem.pre_move_enemy_hero_location())
 end
 
-function test_pre_turn_medium_shadowraze_2_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 0,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 0,
+function test_pre_move_enemy_hero_location_2_fails()
+  cyclone_and_requiem.test_SetGameState({
+    [gs.EH_HAS_CYCLONE_MODIFIER] = 1,
+    [gs.BOT_CASTABLE_REQUIEM] = 0,
+    [gs.BOT_IN_EH_LOCATION] = 0,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_medium_shadowraze())
+  luaunit.assertFalse(cyclone_and_requiem.pre_move_enemy_hero_location())
 end
 
-function test_pre_turn_medium_shadowraze_3_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 0,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 0,
+function test_pre_move_enemy_hero_location_3_fails()
+  cyclone_and_requiem.test_SetGameState({
+    [gs.EH_HAS_CYCLONE_MODIFIER] = 1,
+    [gs.BOT_CASTABLE_REQUIEM] = 1,
+    [gs.BOT_IN_EH_LOCATION] = 1,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_medium_shadowraze())
-end
-
-function test_pre_turn_medium_shadowraze_4_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 1,
-    [gs.BOT_IS_TURNING] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_medium_shadowraze())
-end
-
-function test_pre_turn_medium_shadowraze_5_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_medium_shadowraze())
-end
-
----------------------------------
-
-function test_pre_turn_far_shadowraze_1_succeed()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 0,
-  })
-
-  luaunit.assertTrue(nuke_enemy_hero.pre_turn_far_shadowraze())
-end
-
-function test_pre_turn_far_shadowraze_1_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 0,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_far_shadowraze())
-end
-
-function test_pre_turn_far_shadowraze_2_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 0,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_far_shadowraze())
-end
-
-function test_pre_turn_far_shadowraze_3_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 0,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_far_shadowraze())
-end
-
-function test_pre_turn_far_shadowraze_4_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 1,
-    [gs.BOT_IS_TURNING] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_far_shadowraze())
-end
-
-function test_pre_turn_far_shadowraze_5_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-    [gs.BOT_IS_TURNING] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_turn_far_shadowraze())
-end
-
----------------------------------
-
-function test_pre_near_shadowraze_1_succeed()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 1,
-  })
-
-  luaunit.assertTrue(nuke_enemy_hero.pre_near_shadowraze())
-end
-
-function test_pre_near_shadowraze_1_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 0,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_near_shadowraze())
-end
-
-function test_pre_near_shadowraze_2_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 0,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_near_shadowraze())
-end
-
-function test_pre_near_shadowraze_3_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 0,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_near_shadowraze())
-end
-
-function test_pre_near_shadowraze_4_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_NEAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_NEAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH_MELEE_RANGE] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_near_shadowraze())
-end
-
----------------------------------
-
-function test_pre_medium_shadowraze_1_succeed()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 1,
-  })
-
-  luaunit.assertTrue(nuke_enemy_hero.pre_medium_shadowraze())
-end
-
-function test_pre_medium_shadowraze_1_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 0,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_medium_shadowraze())
-end
-
-function test_pre_medium_shadowraze_2_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 0,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_medium_shadowraze())
-end
-
-function test_pre_medium_shadowraze_3_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 0,
-    [gs.BOT_IS_FACING_EH] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_medium_shadowraze())
-end
-
-function test_pre_medium_shadowraze_4_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_MEDIUM_SHADOWRAZE] = 1,
-    [gs.EH_IN_MEDIUM_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_medium_shadowraze())
-end
-
----------------------------------
-
-function test_pre_far_shadowraze_1_succeed()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 1,
-  })
-
-  luaunit.assertTrue(nuke_enemy_hero.pre_far_shadowraze())
-end
-
-function test_pre_far_shadowraze_1_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 0,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_far_shadowraze())
-end
-
-function test_pre_far_shadowraze_2_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 0,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_far_shadowraze())
-end
-
-function test_pre_far_shadowraze_3_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 0,
-    [gs.BOT_IS_FACING_EH] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_far_shadowraze())
-end
-
-function test_pre_far_shadowraze_4_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_FAR_SHADOWRAZE] = 1,
-    [gs.EH_IN_FAR_SHADOWRAZE_RANGE] = 1,
-    [gs.BOT_IS_FACING_EH] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_far_shadowraze())
+  luaunit.assertFalse(cyclone_and_requiem.pre_move_enemy_hero_location())
 end
 
 ---------------------------------
 
 function test_pre_requiem_1_succeed()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_REQUIEM ] = 1,
-    [gs.EH_HAS_HP_FOR_REQUIEM] = 1,
-    [gs.EH_CAN_EVADE_REQUIEM] = 0,
+  cyclone_and_requiem.test_SetGameState({
+    [gs.BOT_CASTABLE_REQUIEM] = 1,
+    [gs.BOT_IN_EH_LOCATION] = 1,
   })
 
-  luaunit.assertTrue(nuke_enemy_hero.pre_requiem())
+  luaunit.assertTrue(cyclone_and_requiem.pre_requiem())
 end
 
 function test_pre_requiem_1_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 0,
-    [gs.BOT_CASTABLE_REQUIEM ] = 1,
-    [gs.EH_HAS_HP_FOR_REQUIEM] = 1,
-    [gs.EH_CAN_EVADE_REQUIEM] = 0,
+  cyclone_and_requiem.test_SetGameState({
+    [gs.BOT_CASTABLE_REQUIEM] = 0,
+    [gs.BOT_IN_EH_LOCATION] = 1,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_requiem())
+  luaunit.assertFalse(cyclone_and_requiem.pre_requiem())
 end
 
 function test_pre_requiem_2_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_REQUIEM ] = 0,
-    [gs.EH_HAS_HP_FOR_REQUIEM] = 1,
-    [gs.EH_CAN_EVADE_REQUIEM] = 0,
+  cyclone_and_requiem.test_SetGameState({
+    [gs.BOT_CASTABLE_REQUIEM] = 1,
+    [gs.BOT_IN_EH_LOCATION] = 0,
   })
 
-  luaunit.assertFalse(nuke_enemy_hero.pre_requiem())
-end
-
-function test_pre_requiem_3_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_REQUIEM ] = 1,
-    [gs.EH_HAS_HP_FOR_REQUIEM] = 0,
-    [gs.EH_CAN_EVADE_REQUIEM] = 0,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_requiem())
-end
-
-function test_pre_requiem_4_fails()
-  nuke_enemy_hero.test_SetGameState({
-    [gs.EH_IS_VISIBLE] = 1,
-    [gs.BOT_CASTABLE_REQUIEM ] = 1,
-    [gs.EH_HAS_HP_FOR_REQUIEM] = 1,
-    [gs.EH_CAN_EVADE_REQUIEM] = 1,
-  })
-
-  luaunit.assertFalse(nuke_enemy_hero.pre_requiem())
+  luaunit.assertFalse(cyclone_and_requiem.pre_requiem())
 end
 
 os.exit(luaunit.LuaUnit.run())
